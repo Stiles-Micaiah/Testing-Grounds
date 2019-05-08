29938e6be3babef66fd463b9c0a960fee3aab2b2
Judah <syluxprime40@gmail.com>
2019-05-08 14:05:15 -0600

    bullshit

---
+++ app/ground-zero.js
@@ -0,0 +1,14 @@
+function jsUcfirst(string, position) 
+{
+  return string[position] + string[position].toUpperCase()
+   
+    }
+    
+     var  getFucked = "qwertyu"
+     
+     console.log(3)
+     
+     
+     var bullshit = 23
+     
+jsUcfirst(getFucked, 4)
